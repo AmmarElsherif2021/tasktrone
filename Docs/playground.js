64556b30dd51f6ff50103f95de2478341a9d9c21
AmmarElsherif2021 <ammar.alshareef2014@gmail.com>
2026-05-07 17:30:10 +0300

    Article4-DFD completed

--- Docs/playground.js
+++ Docs/playground.js
@@ -1,10 +1,12 @@
-import { readFile } from 'fs/promises';
+import { readFile,writeFile } from 'fs/promises';
 
 const data = JSON.parse(await readFile(new URL('./enums.json', import.meta.url), 'utf8'));
 const enumsMap = new Map();
 for(const [key, value] of Object.entries(data)) {
   if(enumsMap.has(value['enum_type_name'])) {
-    enumsMap.get(value['enum_type_name']).push(value['enum_value']);
+    if(!enumsMap.get(value['enum_type_name']).includes(value['enum_value'])) {
+      enumsMap.get(value['enum_type_name']).push(value['enum_value']);
+    }
   } else {
     enumsMap.set(value['enum_type_name'], [value['enum_value']]);
   }
